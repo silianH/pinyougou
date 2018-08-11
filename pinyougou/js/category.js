9c2b3b5d2ef9a616be87a3bbf75c7442a2e08efb
silianH <838716861@qq.com>
2018-08-11 10:02:39 +0800

    rem改造

--- pinyougou/js/category.js
+++ pinyougou/js/category.js
@@ -2,24 +2,29 @@ $(function () {
   init();
 
   function init() {
-    var myScroll;
-    
     getCategories();
   }
   // 滚动插件
   function loaded() {
-    myScroll = new IScroll('#leftScroll');
-    myScroll1 = new IScroll('#rightScroll');
+    // myScroll1 = new IScroll('#rightScroll');
+    // $("img").on("load", function () {
+    //   rightScroll = new IScroll('#rightScroll');
+    // });
+    leftScroll = new IScroll('#leftScroll');
   }
 
   function getCategories() {
     // http://api.pyg.ak48.xyz/api/public/v1/categories
     $.get("http://api.pyg.ak48.xyz/api/public/v1/categories", function (res) {
       // console.log(res);
-      var categoriesTemp = template("categoriesTemplate",{data:res.data});
+      var categoriesTemp = template("categoriesTemplate", {
+        data: res.data
+      });
       // console.log(categoriesTemp);
       $(".left").html(categoriesTemp);
-      var cgContentTemp = template("cgContentTemplate",{data:res.data});
+      var cgContentTemp = template("cgContentTemplate", {
+        data: res.data
+      });
       // console.log(cgContentTemp);
       $(".right").html(cgContentTemp);
       clickCategories();
@@ -27,8 +32,9 @@ $(function () {
     });
   }
   var CategoriesIndex = 0;
+
   function clickCategories() {
-    $(".left>ul>li").on("tap",function () {
+    $(".left>ul>li").on("tap", function () {
       // console.log($(this).index());
       CategoriesIndex = $(this).index();
       console.log(CategoriesIndex);
